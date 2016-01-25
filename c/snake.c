507b0a617d877ade2474a42a4e88aa6e93d4cde0
Michał Budzyński <michal@virtualdesign.pl>
2016-01-25 02:13:43 +0100

    [C] - :notebook: draw the board

--- c/snake.c
+++ c/snake.c
@@ -1,13 +1,31 @@
 #include <ncurses.h>
 #include <unistd.h>
 
+#define DELAY 30000
+
+// Game config
+#define WIDTH 40
+#define HEIGHT 20
+
 int main(int argc, char *argv[]) {
+  int i = 0, j = 0;
 
   initscr();
   noecho();
   curs_set(FALSE);
+  clear();
 
+  for (i=0; i < WIDTH; i++) {
+    for (j=0; j < HEIGHT; j++) {
+      if (
+        (j == 0 | j == HEIGHT - 1) |
+        (i == 0 | i == WIDTH - 1)) {
+        mvprintw(j, i, "#");
+      }
+    }
+  }
+
+  refresh();
   sleep(1);
-
-  endwin(); // Restore normal terminal behavior
+  endwin();
 }
