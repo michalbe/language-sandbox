e2a9a80175aa38304cc5765e3f42110dab8b9aad
Michał Budzyński <michal@virtualdesign.pl>
2016-01-25 01:56:38 +0100

    [C] - :nose: makefile for C and simple ncurses demo

---
+++ c/snake.c
@@ -0,0 +1,13 @@
+#include <ncurses.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+
+  initscr();
+  noecho();
+  curs_set(FALSE);
+
+  sleep(1);
+
+  endwin(); // Restore normal terminal behavior
+}
