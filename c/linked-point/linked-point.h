4fb556263edba7652d7a183e60510d0a2fcca0ad
Michał Budzyński <michal@virtualdesign.pl>
2016-02-20 22:55:13 +0100

    [C] - :fireworks: helper comments

--- c/linked-point/linked-point.h
+++ c/linked-point/linked-point.h
@@ -10,7 +10,9 @@
 void push(struct Point **item, int x, int y);
 int pop(struct Point **item, int *x, int *y);
 int last(struct Point **item, int *x, int *y);
+// remove & return first
 int shift(struct Point **head, int *x, int *y);
+// push as first
 void unshift(struct Point** head, int x, int y);
 
 #endif
